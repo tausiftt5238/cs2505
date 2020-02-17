6b1c36e922cd2a199ad8924d141e0141de91cac0
mohammad Tahsin Tausif <tausiftt5238@gmail.com>
2020-02-17 16:17:45 -0500

    changed the code into a error-free one

--- 2_17_2020/test.c
+++ 2_17_2020/test.c
@@ -13,8 +13,9 @@ void bar(int a[], int size){
 //If you want to return an array, make sure you do it dynamically. Usually you don't wanna return local arrays, because they
 //should be lost after they get outside the scope.
 int* foo_bar(){
-	//int a* = malloc(100* sizeof(int));
-	//int a[100] = {1,23,4,5,6,7};
+	//int a* = malloc(100* sizeof(int));	
+	//int a[100] = {1,23,4,5,6,7};  YOU SHOULD AVOID DOING THIS, BECAUSE DUE TO BLOCK SCOPE, THE LOCAL VARIABLE WILL BE LOST AFTER THE FUNCTION IS RETURNED.
+	// In the class, I wrote int a*. that was wrong. It should be "* a" during declaration. When you want to access it, it should be a*. 
 	int* a = (int*)malloc(100 * sizeof(int));
 	return a;
 }
@@ -29,6 +30,7 @@ int main(){
 		printf("%d\n", c[i]);
 	}
 	bar(c,100);	
+	// As you can see, here the values of the array changes.
 	for(int i =0; i < 5; i++){
 		printf("%d\n", c[i]);
 	}	
