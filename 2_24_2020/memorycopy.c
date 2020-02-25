49b5ccaa9a988d4170ad633129a3d756accd0887
tausiftt5238 <tausiftt5238@gmail.com>
2020-02-25 14:59:53 -0500

    added comments for 2/24/2020

--- 2_24_2020/memorycopy.c
+++ 2_24_2020/memorycopy.c
@@ -6,8 +6,8 @@ int main(){
 	char b[7] = "foobar";
 	printf("%s\n", a);
 	printf("%s\n", b);
-	strncpy(a,b,2);
-	printf("%s\n", a);
-	printf("%s\n", b);
+	strncpy(a,b,2);		// copies content of b to a. 
+	printf("%s\n", a);	// "follo world\n", because we tried to copy 2 characters from b.
+	printf("%s\n", b);  
 	return 0;
 }
--- 2_24_2020/strcm.c
+++ 2_24_2020/strcm.c
@@ -5,8 +5,14 @@ int main(){
 	char a[] = "hello";
 	char b[] = "hello";
 	char c[] = "world";
-	strncmp(a,b,5);
-	strncat(a,b,5);
-	strcat(strcat(a,b),c);
-	strlen(a); 
+	strcmp(a,b); //compares a against b. The whole string in this case.
+	strncmp(a,b,5);	//compares 5 letters of the two strings
+
+	strcat(a,b); //copies content of b, and puts them at the end of a. In this case, output will be "hellohello". it did a + b.
+	strncat(a,b,5);	//copies 5 letters from the 2nd parameter (variable b in this case) to first parameter (a in this case)
+	strcat(strcat(a,b),c); // if you want to concat multiple strings at once
+	
+	strlen(a); 	//to find out length of the string. It returns the number of characters before the null terminator (0 or '\0' )
 	char x[10]; 
+	return 0;
+}
